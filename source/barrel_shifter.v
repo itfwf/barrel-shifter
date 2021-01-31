e00fd8776cfda69d45cfedde0581f043d432a893
itfwf <ioan.tugui@gmail.com>
2021-01-31 14:20:33 +0200

    _

--- source/barrel_shifter.v
+++ source/barrel_shifter.v
@@ -16,25 +16,20 @@ end
 
 always @(posedge CK)
 begin
-
-if(p_load==1'b1 )
-begin	
-
-q=in;
-	if(shift_l_r==1'b0)
-	begin
-		for(index = 0; index <shift_by; index = index + 1) begin
-    			q={q[6:0],q[7]};
-  		end
-	end
+	if(p_load==1'b1 ) begin	
+		q=in;
+		if(shift_l_r==1'b0)
+		begin
+			for(index = 0; index <shift_by; index = index + 1) begin
+    				q={q[6:0],q[7]};
+  			end
+		end
 	else begin
 		for(index = 0; index <shift_by; index = index + 1) begin
     			q={q[0],q[7:1]};
   		end
+		end
 	end
-	
-
-end
 end
 assign out=q;
 endmodule
